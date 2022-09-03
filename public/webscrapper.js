554cd53d3d1fd7b7b4943f403f32734e393c7c5c
palak <palakhandelwalgp2gmail.com>
2022-09-04 02:04:15 +0530

    added web scrapper

---
+++ public/webscrapper.js
@@ -0,0 +1,34 @@
+const cheerio= require("cheerio");
+const request= require("request");
+
+
+// request('https://leetcode.com/chaharnishant/',(error,response,html)=>{
+//     if(!error && response.statusCode==200)
+//     {
+//         const $= cheerio.load(html);
+//         const total_solved=$('.text-[24px] font-medium text-label-1 dark:text-dark-label-1');
+//         console.log(total_solved.html());
+//     }
+// })
+
+
+request('https://www.codechef.com/users/nishant403',(error,response, html)=>{
+    if(!error && response.statusCode==200)
+    {
+        const $ = cheerio.load(html);
+        const rating= $('.rating-number');
+        console.log("Current rating of codechef is "+ rating.html());
+
+    }
+})
+
+
+request('https://codeforces.com/profile/nishant403',(error,response, html)=>{
+    if(!error && response.statusCode==200)
+    {
+        const $ = cheerio.load(html);
+        const rating= $('ul li span .user-red');
+        console.log("Current rating of codeforces is "+ rating.html());
+
+    }
+})
